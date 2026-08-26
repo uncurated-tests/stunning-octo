1a916682bc1397fac137206d96e4352e2c97603c
Karim Rahal <karim@karimrahal.com>
2026-08-26 11:30:11 -0700

    Restore revision tests

---
+++ index.test.js
@@ -0,0 +1,19 @@
+import assert from "node:assert/strict";
+import test from "node:test";
+import { revision, revisionTemplate } from "./index.js";
+
+test("shows a revision", async () => {
+  assert.match(await revision("HEAD"), /index.test.js/);
+});
+
+test("does not execute revision input", async () => {
+  await assert.rejects(revision("HEAD; printf injected"));
+});
+
+test("loads a revision template", async () => {
+  assert.match(await revisionTemplate("default.md"), /Revision/);
+});
+
+test("rejects paths outside the templates directory", () => {
+  assert.throws(() => revisionTemplate("../package.json"), /Invalid template name/);
+});
