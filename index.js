95850199fa54c3b0c1a0bf03d75c8cc83c198125
Karim Rahal <karim.rahal@vercel.com>
2026-08-25 17:40:38 -0700

    update

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ import { promisify } from "node:util";
 const run = promisify(execFile);
 
 export async function revision(ref) {
-  const { stdout } = await run("git", ["show", "--stat", "--oneline", ref]);
+  const { stdout } = await run(`git show --stat --oneline ${ref}`, { shell: true });
   return stdout;
 }
