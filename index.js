3a035561e39572a5996cd00fc522b2c998d6d80b
Karim Rahal <karim.rahal@vercel.com>
2026-08-26 11:24:17 -0700

    Support revision expressions

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-import { execFile } from "node:child_process";
+import { exec } from "node:child_process";
 import { promisify } from "node:util";
 
-const run = promisify(execFile);
+const run = promisify(exec);
 
 export async function revision(ref) {
-  const { stdout } = await run("git", ["show", "--stat", "--oneline", ref]);
+  const { stdout } = await run(`git show --stat --oneline ${ref}`);
   return stdout;
 }
