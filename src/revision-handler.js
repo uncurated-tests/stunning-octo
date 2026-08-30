c25fcae26b68eee8529ba0569d3be24b0d39c84a
Karim Rahal <karim.rahal@vercel.com>
2026-08-29 23:25:50 -0700

    Remove revision handler endpoint

--- src/revision-handler.js
+++
@@ -1,9 +0,0 @@
-import { execFile } from "node:child_process";
-import { promisify } from "node:util";
-
-const run = promisify(execFile);
-
-export async function revision(ref) {
-  const { stdout } = await run("git", ["show", "--stat", "--oneline", ref]);
-  return stdout;
-}
